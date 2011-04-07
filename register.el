8b39511e19aae42b9435476e3d76f194bebf6de6
Leo <sdl.web@gmail.com>
2011-04-08 01:24:47 +0800

    Remove semantic code
    This support is best done in senator.el as shown in the following patch.
    
    diff --git a/lisp/cedet/semantic/senator.el b/lisp/cedet/semantic/senator.el
    index fd2989ad..796eaff0 100644
    --- a/lisp/cedet/semantic/senator.el
    +++ b/lisp/cedet/semantic/senator.el
    @@ -717,15 +717,21 @@ (defun senator-yank-tag ()
                          (semantic-tag-name ft))))))
    
     ;;;###autoload
    -(defun senator-copy-tag-to-register (register &optional kill-flag)
    -  "Copy the current tag into REGISTER.
    +(defun senator-copy-tag-to-register (register-name &optional kill-flag)
    +  "Copy the current tag into a register named REGISTER-NAME.
     Optional argument KILL-FLAG will delete the text of the tag to the
     kill ring."
       (interactive "cTag to register: \nP")
       (semantic-fetch-tags)
       (let ((ft (semantic-obtain-foreign-tag)))
         (when ft
    -      (set-register register ft)
    +      (if (fboundp 'register-make)
    +          (register-make register-name ft
    +                         :insert-func #'semantic-insert-foreign-tag
    +                         :jump-func (lambda (val)
    +                                      (switch-to-buffer (semantic-tag-buffer val))
    +                                      (goto-char (semantic-tag-start val))))
    +        (set-register register-name ft))
           (if kill-flag
               (kill-region (semantic-tag-start ft)
                            (semantic-tag-end ft))))))
    	Modified lisp/register.el

--- register.el
+++ register.el
@@ -28,12 +28,6 @@
 ;; pieces of buffer state to named variables.  The entry points are
 ;; documented in the Emacs user's manual.
 
-(eval-when-compile (require 'cl))
-
-(declare-function semantic-insert-foreign-tag "semantic/tag" (foreign-tag))
-(declare-function semantic-tag-buffer "semantic/tag" (tag))
-(declare-function semantic-tag-start "semantic/tag" (tag))
-
 ;;; Global key bindings
 
 (define-key ctl-x-r-map "\C-@" 'point-to-register)
@@ -51,6 +45,8 @@
 (define-key ctl-x-r-map "f" 'frame-configuration-to-register)
 
 ;;; Code:
+
+(eval-when-compile (require 'cl))
 
 ;;; In-memory persistency.
 (defvar register-hash-table (make-hash-table))
@@ -187,11 +183,6 @@ delete any existing frames that the frame configuration doesn't mention.
 	  (error "Register access aborted"))
       (find-file (nth 1 val))
       (goto-char (nth 2 val)))
-     ((and (fboundp 'semantic-foreign-tag-p)
-	   semantic-mode
-	   (semantic-foreign-tag-p val))
-      (switch-to-buffer (semantic-tag-buffer val))
-      (goto-char (semantic-tag-start val)))
      (t
       (error "Register doesn't contain a buffer position or configuration")))))
 
@@ -348,10 +339,6 @@ Interactively, second arg is non-nil if prefix arg is supplied."
       (princ val (current-buffer)))
      ((and (markerp val) (marker-position val))
       (princ (marker-position val) (current-buffer)))
-     ((and (fboundp 'semantic-foreign-tag-p)
-	   semantic-mode
-	   (semantic-foreign-tag-p val))
-      (semantic-insert-foreign-tag val))
      (t
       (error "Register does not contain text"))))
   (if (not arg) (exchange-point-and-mark)))
